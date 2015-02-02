517e8f5dada4d3eecf4a8a8316a9115541466735
Emma Kuo <emma.c.kuo@gmail.com>
2015-02-01 22:11:12 -0800

    improve line editor

--- nekocafe.js
+++ nekocafe.js
@@ -1,10 +1,52 @@
-var net = require('net');
-var readline = require('readline');
+var telnet = require('telnet');
+var keypress = require('keypress');
+var ansi = require('ansi');
 
-var Neko = function (sock, name, rl) {
-    this.sock = sock;
+var Prompter = function (client, prompt) {
+    var self = this;
+    this.client = client;
+    this.prompt = prompt;
+    this.buf = '';
+    this.cursor = ansi(client, {enabled: true});
+    this.onInput = null;
+
+    this.write = function(msg) {
+        self.cursor.horizontalAbsolute(0).eraseLine();
+        self.cursor.write(msg);
+        self.cursor.write(self.prompt + self.buf);
+    };
+
+    this.handleKey = function(ch, key) {
+        if (ch !== undefined && ch >= ' ' && ch <= '~') {
+            self.cursor.write(ch);
+            self.buf += ch;
+        } else if (key.name === 'backspace' && self.buf.length > 0) {
+            self.cursor.back(1);
+            self.cursor.write(' ');
+            self.cursor.back(1);
+            self.buf = self.buf.slice(0, -1);
+        } else if (key.name === 'return' && self.buf.length > 0) {
+            if (self.onInput !== null) {
+                self.onInput(self.buf);
+            }
+            self.buf = '';
+            self.cursor.horizontalAbsolute(0).eraseLine().write(self.prompt);
+        }
+    };
+
+    client.do.transmit_binary();
+    client.do.suppress_go_ahead();
+    client.will.suppress_go_ahead();
+    client.will.echo();
+    keypress(client);
+    client.on('keypress', this.handleKey);
+    this.cursor.write(this.prompt);
+};
+
+var Neko = function (prompter, name) {
+    var self = this;
+    this.prompter = prompter;
     this.name = name;
-    this.rl = rl;
 };
 
 var Cafe = function (port) {
@@ -12,49 +54,49 @@ var Cafe = function (port) {
     this.port = port;
     this.nekoes = [];
 
-    this.describe = function(sock) {
-        sock.write(
-            'You are in Neko Cafe.\r\n'
-            + 'Nekoes here: '
-            + this.nekoes.map(function(n) {return n.name;}).join(', ')
-            + '.\r\n');
+    this.description = function() {
+        return 'You are in Neko Cafe.\r\n' +
+            'Nekoes here: ' +
+            this.nekoes.map(function(n) {return n.name;}).join(', ') +
+            '.\r\n';
     };
 
-    this.signIn = function(sock) {
-        var rl = readline.createInterface(sock, sock);
-        rl.question('What is your neko name? ', function(name) {
-            self.addNeko(new Neko(sock, name, rl));
-            self.describe(sock);
-        });
+    this.signIn = function(client) {
+        var prompter = new Prompter(client, 'What is your name? ');
+        prompter.onInput = function(name) {
+            self.addNeko(new Neko(prompter, name));
+            prompter.prompt = '> ';
+        };
     };
 
     this.writeNekoes = function(msg) {
         this.nekoes.forEach(function(n) {
-            n.sock.write(msg + '\r\n');
+            n.prompter.write(msg);
         });
     };
 
     this.addNeko = function(neko) {
         this.nekoes.push(neko);
-        this.writeNekoes('* ' + neko.name + ' joined.');
-        neko.rl.on('line', function(line) {
-            if (line === '/look')
-                self.describe(neko.sock);
-            else if (line === '/quit')
-                neko.sock.end();
+        this.writeNekoes('* ' + neko.name + ' joined.\r\n');
+        neko.prompter.write(self.description());
+        neko.prompter.onInput = function(input) {
+            if (input === '/look')
+                neko.prompter.write(self.description());
+            else if (input === '/quit')
+                neko.prompter.client.end();
             else
-                self.writeNekoes('<' + neko.name + '> ' + line);
-        });
-        neko.sock.on('end', function() {
+                self.writeNekoes('<' + neko.name + '> ' + input + '\r\n');
+        };
+        neko.prompter.client.on('end', function() {
             self.nekoes = self.nekoes.filter(function(n) {
                 return n.name !== neko.name;
             });
-            self.writeNekoes('* ' + neko.name + ' left.');
+            self.writeNekoes('* ' + neko.name + ' left.\r\n');
         });
     };
 
     this.start = function() {
-        net.createServer(this.signIn)
+        telnet.createServer(this.signIn)
             .listen(this.port, function () {
                 console.log('Listening on port ' + self.port);
         });
