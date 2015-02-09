c3e415d42f959539247e0f57f0140e199053674d
Emma Kuo <emma.c.kuo@gmail.com>
2015-02-08 23:08:38 -0800

    socket.io experimentation

--- nekocafe.js
+++ nekocafe.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static('static'));
 
-var server = app.listen(10300, function () {
+io.on('connection', function(socket) {
+    console.log('user connected');
+    socket.send('Hello socket');
+});
+
+var server = http.listen(10300, function () {
     var address = server.address();
     console.log('Listening on %s:%s', address.address,
         address.port);
