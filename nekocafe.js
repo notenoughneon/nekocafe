3e2c21bbaa0c20717683dfce59be80ccca8d6071
Emma Kuo <emma.c.kuo@gmail.com>
2015-02-09 23:37:39 -0800

    minimum viable websock client

--- nekocafe.js
+++ nekocafe.js
@@ -5,9 +5,23 @@ var io = require('socket.io')(http);
 
 app.use(express.static('static'));
 
+var allnicks = [];
+
 io.on('connection', function(socket) {
-    console.log('user connected');
-    socket.send('Hello socket');
+    var mynick;
+    socket.on('nick', function(nick) {
+        mynick = nick;
+        allnicks.push(mynick);
+        io.emit('join', mynick);
+        socket.emit('nicks', allnicks);
+    });
+    socket.on('disconnect', function() {
+        allnicks = allnicks.filter(function(n) {return n !== mynick;});
+        io.emit('part', mynick);
+    });
+    socket.on('message', function(msg) {
+        io.emit('message', {nick: mynick, message: msg});
+    });
 });
 
 var server = http.listen(10300, function () {
